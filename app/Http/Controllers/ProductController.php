d3fd159b82b312b7ea202905f71c1f3161d47d08
Merco <mervicjeazet24@gmail.com>
2024-12-13 15:16:37 +0100

    feat: Create getProducts function for get all products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    //getProduct
+    public function getProducts(): \Illuminate\Http\JsonResponse
+    {
+        return response()->json(Product::all());
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//get all products
+Route::get('products', [\App\Http\Controllers\ProductController::class, 'getProducts']);
